2e2d64cee6baf795eb335a7f459374f4ea61f0e0
Lezixjk <phuc106176@donga.edu.com>
2024-10-18 11:57:13 +0700

    firts commit

---
+++ HealthyDietWeb/home.js
@@ -0,0 +1,17 @@
+const countdown = () => {
+    const eventDate = new Date("Dec 31, 2024 23:59:59").getTime();
+    const now = new Date().getTime();
+    const timeLeft = eventDate - now;
+
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+};
+
+setInterval(countdown, 1000);
